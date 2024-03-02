2a702fa0f2000453ecaf65b45019b1fdac78d97c
Priyanshu Sharma <priyanshusharma72002@gmail.com>
2024-03-03 00:46:57 +0530

    some crud created

---
+++ Promact.CustomerSuccess.Platform/Services/Dtos/ResourcesDtos.cs
@@ -0,0 +1,16 @@
+﻿using Volo.Abp.Application.Dtos;
+
+namespace Promact.CustomerSuccess.Platform.Services.Dtos
+{
+    public class ResourcesDtos : IEntityDto<Guid>
+    {
+        public Guid Id { get; set; }
+        public string ResourceName { get; set; }
+        public string Role { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Comment { get; set; }
+    }
+
+
+}
---
+++ Promact.CustomerSuccess.Platform/Services/Dtos/UpdateResourcesDtos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Promact.CustomerSuccess.Platform.Services.Dtos
+{
+    public class UpdateResourcesDtos
+    {
+        [Required]
+        [StringLength(128)]
+        public string ResourceName { get; set; }
+        public string Role { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Comment { get; set; }
+    }
+}
